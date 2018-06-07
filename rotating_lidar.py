8c62fedbd3fc771af5da36e4e0b5dc08408d81df
Sourish Ghosh <ragnarok0211@gmail.com>
2018-06-07 18:25:57 -0400

    add conv layer

--- rotating_lidar.py
+++ rotating_lidar.py
@@ -7,20 +7,20 @@ import sys
 
 # Constants
 PI = 3.14159
-num_obstacles = 10 
-world_samples = 10 # no of random worlds
-control_samples = 15 # no of observations in each world
+num_obstacles = 10
+world_samples = 10  # no of random worlds
+control_samples = 15  # no of observations in each world
 
 # Control params
-max_angular_velocity = 2.0 # control input sampled from [-max, max]
-init_theta = 0.0 # initial state
-dt = 0.1 # time increment
-T = 10 # total time for one control input
+max_angular_velocity = 2.0  # control input sampled from [-max, max]
+init_theta = 0.0  # initial state
+dt = 0.1  # time increment
+T = 10  # total time for one control input
 
 # LIDAR params
 lidar_pos = (0.0, 0.0, 0.2)
 theta_range = np.deg2rad(120.0)
-num_rays = 100 # discretization of rays
+num_rays = 100  # discretization of rays
 lidar_range = 5.0
 
 # Obstacle params
@@ -31,112 +31,120 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 # Create ground plane
 p.createCollisionShape(p.GEOM_PLANE)
-p.createMultiBody(0,0)
+p.createMultiBody(0, 0)
+
 
 def normAngle(theta):
-  while theta > PI:
-    theta = theta - 2 * PI
-  while theta <= -PI:
-    theta = theta + 2 * PI
-  return theta
+    while theta > PI:
+        theta = theta - 2 * PI
+    while theta <= -PI:
+        theta = theta + 2 * PI
+    return theta
+
 
 def createRandomWorld():
-  radius = 0.2
-  max_x, max_y = 4, 4
-  eps = 0.3
-  x1 = np.random.uniform(-max_x, -eps, num_obstacles/2)
-  x2 = np.random.uniform(eps, max_x, num_obstacles/2)
-  x = np.concatenate((x1, x2), axis=0)
-  y1 = np.random.uniform(-max_y, -eps, num_obstacles/2)
-  y2 = np.random.uniform(eps, max_y, num_obstacles/2)
-  y = np.concatenate((y1, y2), axis=0)
+    radius = 0.2
+    max_x, max_y = 4, 4
+    eps = 0.3
+    x1 = np.random.uniform(-max_x, -eps, num_obstacles / 2)
+    x2 = np.random.uniform(eps, max_x, num_obstacles / 2)
+    x = np.concatenate((x1, x2), axis=0)
+    y1 = np.random.uniform(-max_y, -eps, num_obstacles / 2)
+    y2 = np.random.uniform(eps, max_y, num_obstacles / 2)
+    y = np.concatenate((y1, y2), axis=0)
 
-  obstCylinderId = p.createCollisionShape(p.GEOM_CYLINDER, radius=0.2)
-  UIDs = []
-  for i in range(x.shape[0]):
-    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[x[i],y[i],0], baseOrientation=[0,0,0,1])
-    UIDs.append(obstUID)
-  return UIDs
+    obstCylinderId = p.createCollisionShape(p.GEOM_CYLINDER, radius=0.2)
+    UIDs = []
+    for i in range(x.shape[0]):
+        obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[x[i], y[i], 0],
+                                    baseOrientation=[0, 0, 0, 1])
+        UIDs.append(obstUID)
+    return UIDs
+
 
 def repositionObstacles(UIDs):
-  max_x, max_y = 4, 4
-  eps = 0.3
-  x1 = np.random.uniform(-max_x, -eps, num_obstacles/2)
-  x2 = np.random.uniform(eps, max_x, num_obstacles/2)
-  x = np.concatenate((x1, x2), axis=0)
-  y1 = np.random.uniform(-max_y, -eps, num_obstacles/2)
-  y2 = np.random.uniform(eps, max_y, num_obstacles/2)
-  y = np.concatenate((y1, y2), axis=0)
-  i = 0
-  for id in UIDs:
-    p.resetBasePositionAndOrientation(id, posObj=[x[i],y[i],0], ornObj=[0,0,0,1])
-    i = i + 1
+    max_x, max_y = 4, 4
+    eps = 0.3
+    x1 = np.random.uniform(-max_x, -eps, num_obstacles / 2)
+    x2 = np.random.uniform(eps, max_x, num_obstacles / 2)
+    x = np.concatenate((x1, x2), axis=0)
+    y1 = np.random.uniform(-max_y, -eps, num_obstacles / 2)
+    y2 = np.random.uniform(eps, max_y, num_obstacles / 2)
+    y = np.concatenate((y1, y2), axis=0)
+    i = 0
+    for id in UIDs:
+        p.resetBasePositionAndOrientation(id, posObj=[x[i], y[i], 0], ornObj=[0, 0, 0, 1])
+        i = i + 1
+
 
 def createWorld():
-  '''
+    '''
   Create a fixed world
   '''
-  obstCylinderId = p.createCollisionShape(p.GEOM_CYLINDER, radius=0.3)
-  obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[2.2,1.2,0], baseOrientation=[0,0,0,1])
-  obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[3,1,0], baseOrientation=[0,0,0,1])
-  obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[-3.5,-1,0], baseOrientation=[0,0,0,1])
-  obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[-1,1.5,0], baseOrientation=[0,0,0,1])
-  obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[0,-1.2,0], baseOrientation=[0,0,0,1])
-  obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[4,-2.2,0], baseOrientation=[0,0,0,1])
-  obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[-4,3,0], baseOrientation=[0,0,0,1])
-  obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[3.4,-3.4,0], baseOrientation=[0,0,0,1])
-  obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[-1,1,0], baseOrientation=[0,0,0,1])
+    obstCylinderId = p.createCollisionShape(p.GEOM_CYLINDER, radius=0.3)
+    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[2.2, 1.2, 0], baseOrientation=[0, 0, 0, 1])
+    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[3, 1, 0], baseOrientation=[0, 0, 0, 1])
+    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[-3.5, -1, 0], baseOrientation=[0, 0, 0, 1])
+    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[-1, 1.5, 0], baseOrientation=[0, 0, 0, 1])
+    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[0, -1.2, 0], baseOrientation=[0, 0, 0, 1])
+    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[4, -2.2, 0], baseOrientation=[0, 0, 0, 1])
+    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[-4, 3, 0], baseOrientation=[0, 0, 0, 1])
+    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[3.4, -3.4, 0], baseOrientation=[0, 0, 0, 1])
+    obstUID = p.createMultiBody(mass, obstCylinderId, -1, basePosition=[-1, 1, 0], baseOrientation=[0, 0, 0, 1])
+
 
 def getBatchRayTo(theta):
-  rays = []
-  theta_inc = theta_range / float(num_rays)
-  for i in range(num_rays):
-    ray = (lidar_pos[0] + lidar_range * math.cos(float(theta)), lidar_pos[1] + lidar_range * math.sin(float(theta)), lidar_pos[2])
-    rays.append(ray)
-    theta = normAngle(theta + theta_inc)
-  return rays
+    rays = []
+    theta_inc = theta_range / float(num_rays)
+    for i in range(num_rays):
+        ray = (lidar_pos[0] + lidar_range * math.cos(float(theta)), lidar_pos[1] + lidar_range * math.sin(float(theta)),
+               lidar_pos[2])
+        rays.append(ray)
+        theta = normAngle(theta + theta_inc)
+    return rays
+
 
 def getRangeReading(theta):
-  ranges = []
-  rayFroms = [lidar_pos for i in range(num_rays)]
-  rayTos = getBatchRayTo(theta)
-  rayTestInfo = p.rayTestBatch(rayFroms, rayTos)
-  for i in range(len(rayTestInfo)):
-    # compute hit distance
-    d = float(rayTestInfo[i][2]) * lidar_range
-    # if no hit
-    if rayTestInfo[i][0] == -1:
-      d = lidar_range
-    ranges.append(d)
-  # Draw debug rays
-  # for i in range(len(rayTos)):
-  #   p.addUserDebugLine(lidar_pos, rayTos[i], [1,0,0], lifeTime=0.1)
-  return ranges
+    ranges = []
+    rayFroms = [lidar_pos for i in range(num_rays)]
+    rayTos = getBatchRayTo(theta)
+    rayTestInfo = p.rayTestBatch(rayFroms, rayTos)
+    for i in range(len(rayTestInfo)):
+        # compute hit distance
+        d = float(rayTestInfo[i][2]) * lidar_range
+        # if no hit
+        if rayTestInfo[i][0] == -1:
+            d = lidar_range
+        ranges.append(d)
+    # Draw debug rays
+    # for i in range(len(rayTos)):
+    #   p.addUserDebugLine(lidar_pos, rayTos[i], [1,0,0], lifeTime=0.1)
+    return ranges
 
-p.setGravity(0,0,-9.8)
+
+p.setGravity(0, 0, -9.8)
 p.setRealTimeSimulation(1)
 
 if __name__ == '__main__':
-  # Open data file
-  f = open(sys.argv[1], 'wb')
-  # Sample worlds
-  UIDs = createRandomWorld()
-  for z in range(world_samples):
-    print "Recording world", z
-    # Sample control inputs
-    u = np.random.uniform(-max_angular_velocity, max_angular_velocity, control_samples)
-    for i in range(len(u)):
-      # data stored as: theta_dot, theta, range values...
-      # for each world there are "control_samples" number of rows
-      # so total rows = world_samples * control_samples * T
-      # print("Recording datapoint %i\n" % i)
-      for j in range(T):
-        ranges = getRangeReading(init_theta)
-        f.write("%f %f " % (u[i], init_theta))
-        for k in range(len(ranges)):
-          f.write("%f " % ranges[k])
-        f.write("\n")
-        init_theta = normAngle(init_theta + u[i] * dt)
-    # change the world
-    repositionObstacles(UIDs)
-
+    # Open data file
+    f = open(sys.argv[1], 'wb')
+    # Sample worlds
+    UIDs = createRandomWorld()
+    for z in range(world_samples):
+        print "Recording world", z
+        # Sample control inputs
+        u = np.random.uniform(-max_angular_velocity, max_angular_velocity, control_samples)
+        for i in range(len(u)):
+            # data stored as: theta_dot, theta, range values...
+            # for each world there are "control_samples" number of rows
+            # so total rows = world_samples * control_samples * T
+            # print("Recording datapoint %i\n" % i)
+            for j in range(T):
+                ranges = getRangeReading(init_theta)
+                f.write("%f %f " % (u[i], init_theta))
+                for k in range(len(ranges)):
+                    f.write("%f " % ranges[k])
+                f.write("\n")
+                init_theta = normAngle(init_theta + u[i] * dt)
+        # change the world
+        repositionObstacles(UIDs)
--- vae.py
+++ vae.py
@@ -1,8 +1,9 @@
-from keras.layers import Lambda, Input, Dense
+from keras.layers import Lambda, Input, Dense, Conv1D, UpSampling1D, Flatten
 from keras.models import Model
 from keras.losses import mse, binary_crossentropy
 from keras.utils import plot_model
 from keras import backend as K
+from keras.engine.topology import Layer
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,146 +14,195 @@ import sys
 # Constants and params
 theta_range = np.deg2rad(120.0)
 num_rays = 100
-trained = True
+trained = False
 
-H = 9 # no of past observations
-F = 1 # no of future predictions
+H = 9  # no of past observations
+F = 1  # no of future predictions
 
 training_data_fraction = 0.8
 
+class RotateLIDAR(Layer):
+
+    def __init__(self, num_rays, 
+                 num_past_obs,
+                 num_future_pred,
+                 **kwargs):
+        self.num_rays = num_rays
+        self.num_past_obs = num_past_obs
+        self.num_future_pred = num_future_pred
+        self.output_dim = num_future_pred * num_rays
+        super(RotateLIDAR, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+        self.kernel = self.add_weight(name='kernel', 
+                                      shape=(input_shape[1], self.output_dim),
+                                      initializer='uniform',
+                                      trainable=True)
+        super(RotateLIDAR, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, x):
+        return K.dot(x, self.kernel)
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_dim)
+
+
 def prepareDataset(train_file, test_file):
-  x_train, x_val, y_train, y_val, x_test, y_test = [], [], [], [], [], []
-  
-  if not trained:
-    f1 = open(train_file, "r")
+    x_train, x_val, y_train, y_val, x_test, y_test = [], [], [], [], [], []
+
+    if not trained:
+        f1 = open(train_file, "r")
+        x_raw = []
+        y_raw = []
+        lines = [line.rstrip('\n') for line in f1]
+        for i in range(len(lines) - H - F):
+            print "preparing training data point", i
+            x = []
+            y = []
+            u = []
+            for j in reversed(range(H)):
+                parts = lines[i + j].split(" ")
+                #for k in range(num_rays):
+                #    x.append(float(parts[k + 2]))
+                x.append(parts[2:num_rays+2:1])
+                u.append(float(parts[0]))
+            for j in range(F):
+                parts = lines[i + j + H].split(" ")
+                for k in range(num_rays):
+                    y.append(float(parts[k + 2]))
+            
+            x = np.asarray(x)
+            u = np.asarray([u])
+            x = np.concatenate((x, u.T), axis=1)
+            x = x.flatten('F')
+
+            x_raw.append(x)
+            y_raw.append(y)
+
+        x = np.asarray(x_raw)
+        y = np.asarray(y_raw)
+
+        n = len(lines) - H - F
+        n_train_samples = int(training_data_fraction * n)
+        n_val_samples = n - n_train_samples
+
+        training_idx = np.random.randint(x.shape[0], size=n_train_samples)
+        val_idx = np.random.randint(x.shape[0], size=n_val_samples)
+
+        x_train, x_val = x[training_idx, :], x[val_idx, :]
+        y_train, y_val = y[training_idx, :], y[val_idx, :]
+
+        print("Prepared training dataset!")
+
+    f2 = open(test_file, "r")
     x_raw = []
     y_raw = []
-    lines = [line.rstrip('\n') for line in f1]
-    for i in range(len(lines)-H-F):
-      print "preparing training data point", i
-      x = []
-      y = []
-      u = []
-      for j in range(H):
-        parts = lines[i+j].split(" ")
-        for k in range(num_rays):
-          x.append(float(parts[k+2]))
-        u.append(float(parts[0]))
-      for j in range(F):
-        parts = lines[i+j+H].split(" ")
-        for k in range(num_rays):
-          y.append(float(parts[k+2]))
-      x = x + u
-      x_raw.append(x)
-      y_raw.append(y)
+    lines = [line.rstrip('\n') for line in f2]
+    for i in range(len(lines) - H - F):
+        print "preparing testing data point", i
+        x = []
+        y = []
+        u = []
+        for j in reversed(range(H)):
+            parts = lines[i + j].split(" ")
+            #for k in range(num_rays):
+            #    x.append(float(parts[k + 2]))
+            x.append(parts[2:num_rays+2:1])
+            u.append(float(parts[0]))
+        for j in range(F):
+            parts = lines[i + j + H].split(" ")
+            for k in range(num_rays):
+                y.append(float(parts[k + 2]))
+            
+        x = np.asarray(x)
+        u = np.asarray([u])
+        x = np.concatenate((x, u.T), axis=1)
+        x = x.flatten('F')
 
-    x = np.asarray(x_raw)
-    y = np.asarray(y_raw)
+        x_raw.append(x)
+        y_raw.append(y)
 
-    n = len(lines)-H-F
-    n_train_samples = int(training_data_fraction * n)
-    n_val_samples = n - n_train_samples
+    x_test = np.asarray(x_raw)
+    y_test = np.asarray(y_raw)
 
-    training_idx = np.random.randint(x.shape[0], size=n_train_samples)
-    val_idx = np.random.randint(x.shape[0], size=n_val_samples)
-    
-    x_train, x_val = x[training_idx,:], x[val_idx,:]
-    y_train, y_val = y[training_idx,:], y[val_idx,:]
+    return x_train, y_train, x_val, y_val, x_test, y_test
 
-    print("Prepared training dataset!")
-
-  f2 = open(test_file, "r")
-  x_raw = []
-  y_raw = []
-  lines = [line.rstrip('\n') for line in f2]
-  for i in range(len(lines)-H-F):
-    print "preparing testing data point", i
-    x = []
-    y = []
-    u = []
-    for j in range(H):
-      parts = lines[i+j].split(" ")
-      for k in range(num_rays):
-        x.append(float(parts[k+2]))
-      u.append(float(parts[0]))
-    for j in range(F):
-      parts = lines[i+j+H].split(" ")
-      for k in range(num_rays):
-        y.append(float(parts[k+2]))
-    x = x + u
-    x_raw.append(x)
-    y_raw.append(y)
-
-  x_test = np.asarray(x_raw)
-  y_test = np.asarray(y_raw)    
-
-  return x_train, y_train, x_val, y_val, x_test, y_test
 
 # reparameterization trick
 # instead of sampling from Q(z|X), sample eps = N(0,I)
 # z = z_mean + sqrt(var)*eps
 def sampling(args):
-  """Reparameterization trick by sampling fr an isotropic unit Gaussian.
-  # Arguments:
+    """Reparameterization trick by sampling fr an isotropic unit Gaussian.
+    # Arguments:
       args (tensor): mean and log of variance of Q(z|X)
-  # Returns:
+    # Returns:
       z (tensor): sampled latent vector
-  """
+    """
 
-  z_mean, z_log_var = args
-  batch = K.shape(z_mean)[0]
-  dim = K.int_shape(z_mean)[1]
-  # by default, random_normal has mean=0 and std=1.0
-  epsilon = K.random_normal(shape=(batch, dim))
-  return z_mean + K.exp(0.5 * z_log_var) * epsilon
+    z_mean, z_log_var = args
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
+    # by default, random_normal has mean=0 and std=1.0
+    epsilon = K.random_normal(shape=(batch, dim))
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
 
 def plot_results(models,
                  data,
                  batch_size=128,
                  num_samples=1):
-  #encoder, decoder = models
-  encoder, decoder = models
-  x, y = data
-  theta_inc = theta_range / float(num_rays)
-  for k in range(120, 300, 1):
-    plt.figure()
-    for i in range(num_samples):
-      _, _, z = encoder.predict(np.array([x[k],]), batch_size=batch_size)
-      y_pred = decoder.predict(z, batch_size=batch_size)
-      plt.plot([j * np.rad2deg(theta_inc) for j in range(num_rays)], y_pred[0], 'b.')
-    plt.plot([j * np.rad2deg(theta_inc) for j in range(num_rays)], y[k], 'r.')
-    plt.ylabel("output")
+    # encoder, decoder = models
+    encoder, decoder = models
+    x, y = data
+    theta_inc = theta_range / float(num_rays)
+    for k in range(120, 300, 1):
+        plt.figure()
+        for i in range(num_samples):
+            _, _, z = encoder.predict(np.array([x[k], ]), batch_size=batch_size)
+            y_pred = decoder.predict(z, batch_size=batch_size)
+            plt.plot([j * np.rad2deg(theta_inc) for j in range(num_rays)], y_pred[0], 'b.')
+        plt.plot([j * np.rad2deg(theta_inc) for j in range(num_rays)], y[k], 'r.')
+        plt.ylabel("output")
 
-    '''
-    plt.figure()
-    x_plot = np.asarray(np.split(x[k][:(H*num_rays)], H))
-    for i in range(H):
-      plt.plot([j * np.rad2deg(theta_inc) for j in range(num_rays)], x_plot[i][:num_rays], 'b.')
-    plt.ylabel("input")
-    '''
+        '''
+        plt.figure()
+        x_plot = np.asarray(np.split(x[k][:(H*num_rays)], H))
+        for i in range(H):
+          plt.plot([j * np.rad2deg(theta_inc) for j in range(num_rays)], x_plot[i][:num_rays], 'b.')
+        plt.ylabel("input")
+        '''
 
-    plt.show()
+        plt.show()
+
 
 x_train, y_train, x_val, y_val, x_test, y_test = prepareDataset(sys.argv[1], sys.argv[2])
+
+x_train = np.expand_dims(x_train, axis=2)
+x_val = np.expand_dims(x_val, axis=2)
+x_test = np.expand_dims(x_test, axis=2)
 
 # network parameters
 original_dim = x_test.shape[1]
 output_dim = y_test.shape[1]
 
-input_shape = (original_dim, )
-output_shape = (output_dim, )
-intermediate_dim = 505
+conv1_filters = 10
+dim2 = 202
 batch_size = 128
 latent_dim = 50
-epochs = 30
+epochs = 5
 num_samples = 50
+input_shape = (original_dim,1)
+output_shape = (output_dim,)
 
 # VAE model = encoder + decoder
 # build encoder model
-inputs = Input(shape=input_shape, name='encoder_input')
-x = Dense(intermediate_dim, activation='relu')(inputs)
-z_mean = Dense(latent_dim, name='z_mean')(x)
-z_log_var = Dense(latent_dim, name='z_log_var')(x)
+inputs = Input(shape=(original_dim, 1), name='encoder_input')
+x1 = Conv1D(conv1_filters, 9, strides=9, activation='relu', input_shape=(None, original_dim))(inputs)
+xf = Flatten()(x1)
+x2 = Dense(dim2, activation='relu')(xf)
+z_mean = Dense(latent_dim, name='z_mean')(x2)
+z_log_var = Dense(latent_dim, name='z_log_var')(x2)
 
 # use reparameterization trick to push the sampling out as input
 # note that "output_shape" isn't necessary with the TensorFlow backend
@@ -161,51 +211,53 @@ z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
 # instantiate encoder model
 encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
 encoder.summary()
-#plot_model(encoder, to_file='vae_encoder.png', show_shapes=True)
+# plot_model(encoder, to_file='vae_encoder.png', show_shapes=True)
 
 # build decoder model
 latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-y = Dense(intermediate_dim, activation='relu')(latent_inputs)
-outputs = Dense(output_dim, activation='relu')(y)
+y2 = Dense(dim2, activation='relu')(latent_inputs)
+outputs = Dense(output_dim, activation='relu')(y2)
 
 # instantiate decoder model
 decoder = Model(latent_inputs, outputs, name='decoder')
 decoder.summary()
-#plot_model(decoder, to_file='vae_decoder.png', show_shapes=True)
+# plot_model(decoder, to_file='vae_decoder.png', show_shapes=True)
 
 # instantiate VAE model
 outputs = decoder(encoder(inputs)[2])
 vae = Model(inputs, outputs, name='vae')
 
+
 def vae_loss_function(y_true, y_pred):
-  kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-  kl_loss = K.sum(kl_loss, axis=-1)
-  kl_loss *= -0.5
-  mse_loss = mse(y_true, y_pred)
-  mse_loss *= original_dim
-  return K.mean(mse_loss + kl_loss)
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    mse_loss = mse(y_true, y_pred)
+    mse_loss *= original_dim
+    return K.mean(mse_loss + kl_loss)
+
 
 if __name__ == '__main__':
-  models = (encoder, decoder)
-  test_data = (x_test, y_test)
+    models = (encoder, decoder)
+    test_data = (x_test, y_test)
 
-  if trained:
-    # load weights into new model
-    vae.load_weights("vae_weights.h5")
-    vae.compile(optimizer='adam', loss=vae_loss_function)
-    vae.summary()
-    print("Loaded model from disk")
-  else:
-    vae.compile(optimizer='adam', loss=vae_loss_function)
-    vae.summary()
-    vae.fit(x_train,
-            y_train,
-            epochs=epochs,
-            batch_size=batch_size,
-            validation_data=(x_val, y_val))
-    # serialize weights to HDF5
-    vae.save_weights("vae_weights.h5")
-    print("Saved weights to disk")
+    if trained:
+        # load weights into new model
+        vae.load_weights("vae_weights.h5")
+        vae.compile(optimizer='adam', loss=vae_loss_function)
+        vae.summary()
+        print("Loaded model from disk")
+    else:
+        vae.compile(optimizer='adam', loss=vae_loss_function)
+        vae.summary()
+        vae.fit(x_train,
+                y_train,
+                epochs=epochs,
+                batch_size=batch_size,
+                validation_data=(x_val, y_val))
+        # serialize weights to HDF5
+        vae.save_weights("vae_weights.h5")
+        print("Saved weights to disk")
 
-  #plot_model(vae, to_file='vae.png', show_shapes=True)
-  plot_results(models, test_data, batch_size=batch_size, num_samples=num_samples)
+    # plot_model(vae, to_file='vae.png', show_shapes=True)
+    plot_results(models, test_data, batch_size=batch_size, num_samples=num_samples)
